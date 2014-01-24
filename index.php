dd7a435fea1c2a76f34223ffee11c27dce1bf7f2
John Konolol <nico83110@gmail.com>
2014-01-25 00:30:01 +0100

    base

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<title>WebUI</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/smoothness/jquery-ui-1.10.4.custom.min.css">
+		<script src="js/jquery-1.10.2.js"></script>
+		<script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+		<script>
+			$(function() {
+				$( "#dialog" ).dialog();
+			});
+		</script>
+	</head>
+	<body>
+		<div id="dialog" title="Test">
+			<p>Test</p>
+		</div>
+	</body>
+</html>
